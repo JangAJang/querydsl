975fed74a213a70587ebf785eca34a0d920cab87
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-01 22:45:32 +0900

    refactor: MemberRepositoryTest 테스트를 위한 데이터 추가하는 로직을 BeforeEach메서드로 분리

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,10 +1,13 @@
 package study.querydsl.repository;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.domain.Member;
 import study.querydsl.domain.Team;
@@ -25,6 +28,26 @@ class MemberRepositoryTest {
 
     @Autowired
     private EntityManager em;
+
+    @BeforeEach
+    public void initTestData() {
+        Team team1 = new Team("teamA");
+        Team team2 = new Team("teamB");
+        em.persist(team1);
+        em.persist(team2);
+
+        Member member1 = new Member("member1", 10, team1);
+        Member member2 = new Member("member2", 20, team1);
+        Member member3 = new Member("member3", 30, team2);
+        Member member4 = new Member("member4", 40, team2);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+    }
 
     @Test
     @DisplayName("")
@@ -48,22 +71,7 @@ class MemberRepositoryTest {
     @DisplayName("")
     public void searchTest() throws Exception{
         //given
-        Team team1 = new Team("teamA");
-        Team team2 = new Team("teamB");
-        em.persist(team1);
-        em.persist(team2);
 
-        Member member1 = new Member("member1", 10, team1);
-        Member member2 = new Member("member2", 20, team1);
-        Member member3 = new Member("member3", 30, team2);
-        Member member4 = new Member("member4", 40, team2);
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
-
-        em.flush();
-        em.clear();
         //when
         MemberSearchCondition condition = new MemberSearchCondition();
         condition.setAgeGoe(35);
@@ -74,4 +82,5 @@ class MemberRepositoryTest {
         //then
         assertThat(result.get(0).getUsername()).isEqualTo("member4");
     }
+
 }
