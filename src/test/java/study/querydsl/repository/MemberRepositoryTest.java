618673a19d40d9dcf507b4fa216ce98a27944ca1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-01 22:11:27 +0900

    feat: MemberRepositoryTest 리포지토리 기능을 테스트하기 위한 테스트클래스 생성

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,7 +1,37 @@
 package study.querydsl.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.domain.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class MemberRepositoryTest {
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("")
+    public void basicTest() throws Exception{
+        //given
+        Member member = new Member("testMember", 10);
+
+        //when
+        memberRepository.save(member);
+
+        //then
+        assertThat(memberRepository.findById(member.getId()).get()).isEqualTo(member);
+        assertThat(memberRepository.findAll()).containsExactly(member);
+        assertThat(memberRepository.findByUsername("testMember")).containsExactly(member);
+
+        memberRepository.delete(member);
+        assertThat(memberRepository.findAll().size()).isEqualTo(0);
+    }
 }
