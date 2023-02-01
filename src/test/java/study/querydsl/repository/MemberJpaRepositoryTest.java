d2fdb957bf2a9e4ebb7b2ffb6f0c4754cd6ab0f5
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-01 21:45:21 +0900

    feat: MemberJpaRepositoryTest BooleanBuilder를 이용해 동적 쿼리를 활용한 검색 기능을 검증하는 테스트코드 구현

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,5 +1,6 @@
 package study.querydsl.repository;
 
+import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.domain.Member;
+import study.querydsl.domain.Team;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,6 +22,9 @@ public class MemberJpaRepositoryTest {
 
     @Autowired
     private MemberJpaRepository memberJpaRepository;
+
+    @Autowired
+    private EntityManager em;
 
     @Test
     @DisplayName("")
@@ -33,5 +42,36 @@ public class MemberJpaRepositoryTest {
 
         memberJpaRepository.deleteMember(member);
         assertThat(memberJpaRepository.findAll().size()).isEqualTo(0);
+    }
+    
+    @Test
+    @DisplayName("")        
+    public void searchTest() throws Exception{
+        //given
+        Team team1 = new Team("teamA");
+        Team team2 = new Team("teamB");
+        em.persist(team1);
+        em.persist(team2);
+
+        Member member1 = new Member("member1", 10, team1);
+        Member member2 = new Member("member2", 20, team1);
+        Member member3 = new Member("member3", 30, team2);
+        Member member4 = new Member("member4", 40, team2);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+        //when
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        //then
+        assertThat(result.get(0).getUsername()).isEqualTo("member4");
     }
 }
