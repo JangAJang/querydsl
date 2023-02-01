ba246c5fcfa135b5b67e1731429a1463bcd3536f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-01 21:41:21 +0900

    feat: MemberJpaRepository BooleanBuilder를 이용해 dto를 조회하는 리포지토리 기능 구현

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,15 +1,23 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 import study.querydsl.domain.Member;
 import study.querydsl.domain.QMember;
+import study.querydsl.domain.QTeam;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.util.StringUtils.*;
 import static study.querydsl.domain.QMember.*;
+import static study.querydsl.domain.QTeam.*;
 
 @Repository
 public class MemberJpaRepository {
@@ -44,5 +52,29 @@ public class MemberJpaRepository {
     //Delete
     public void deleteMember(Member deleteMember){
         queryFactory.delete(member).where(member.id.eq(deleteMember.getId())).execute();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition memberSearchCondition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(createBuilderForSearch(memberSearchCondition))
+                .fetch();
+    }
+
+    private BooleanBuilder createBuilderForSearch(MemberSearchCondition condition){
+        BooleanBuilder builder = new BooleanBuilder();
+        if(hasText(condition.getUsername())) builder.and(member.username.eq(condition.getUsername()));
+        if(hasText(condition.getTeamName())) builder.and(team.name.eq(condition.getTeamName()));
+        if(condition.getAgeGoe() != null) builder.and(member.age.goe(condition.getAgeGoe()));
+        if(condition.getAgeLoe() != null) builder.and(member.age.loe(condition.getAgeLoe()));
+        return builder;
     }
 }
