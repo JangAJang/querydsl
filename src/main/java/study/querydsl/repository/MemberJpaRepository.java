b5be12e9383df145581c0b6e18992283c3c99f1e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-01 19:06:20 +0900

    feat: MemberJpaRepository CRUD 기능을 수행하는 멤버 리포지토리를 querydsl로 구현

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -3,6 +3,13 @@ package study.querydsl.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import study.querydsl.domain.Member;
+import study.querydsl.domain.QMember;
+
+import java.util.List;
+import java.util.Optional;
+
+import static study.querydsl.domain.QMember.*;
 
 @Repository
 public class MemberJpaRepository {
@@ -13,5 +20,29 @@ public class MemberJpaRepository {
     public MemberJpaRepository(EntityManager em) {
         this.em = em;
         queryFactory = new JPAQueryFactory(em);
+    }
+
+    //Create
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    //Read
+    public Member findById(Long id){
+        return queryFactory.selectFrom(member).where(member.id.eq(id)).fetchOne();
+    }
+
+    public List<Member> findAll(){
+        return queryFactory.selectFrom(member).fetch();
+    }
+
+    public List<Member> findByUsername(String username){
+        return queryFactory.selectFrom(member).where(member.username.eq(username)).fetch();
+    }
+
+    //Update는 변경감지로 실행된다.
+    //Delete
+    public void deleteMember(Member deleteMember){
+        queryFactory.delete(member).where(member.id.eq(deleteMember.getId())).execute();
     }
 }
