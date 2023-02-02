c08404364f78580351a3a2d08f7d37c0976999f2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-02 16:32:42 +0900

    feat: Querydsl4Repository 클래스를 생성하고 인스턴스를 초기화시키는 기능 구현

---
+++ src/main/java/study/querydsl/repository/Querydsl4Repository.java
@@ -0,0 +1,39 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.EntityPath;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.JpaEntityInformationSupport;
+import org.springframework.data.jpa.repository.support.Querydsl;
+import org.springframework.data.querydsl.SimpleEntityPathResolver;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+@Repository
+public class Querydsl4Repository {
+
+    private final Class domainClass;
+    private Querydsl querydsl;
+    private EntityManager entityManager;
+    private JPAQueryFactory queryFactory;
+
+    public Querydsl4Repository(Class<?> domainClass) {
+        Assert.notNull(domainClass, "Domain Class must not be null");
+        this.domainClass = domainClass;
+    }
+
+    @Autowired
+    public void setEntityManager(EntityManager entityManager){
+        Assert.notNull(domainClass, "Entity Manager must not be null");
+        JpaEntityInformation entityInformation = JpaEntityInformationSupport
+                .getEntityInformation(domainClass, entityManager);
+        SimpleEntityPathResolver resolver = SimpleEntityPathResolver.INSTANCE;
+        EntityPath path = resolver.createPath(entityInformation.getJavaType());
+        this.entityManager = entityManager;
+        this.querydsl = new Querydsl(entityManager, new PathBuilder<>(path.getType(), path.getMetadata()));
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+}
