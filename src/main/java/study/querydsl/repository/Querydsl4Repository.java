6c108b9d56041133a80904b19ea7564fd1da73b6
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-02 16:34:16 +0900

    feat: Querydsl4Repository 생성자가 만들어지고 모든 인스턴스가 초기화되었을 떄 널값인지를 검증하는 기능 구현

--- src/main/java/study/querydsl/repository/Querydsl4Repository.java
+++ src/main/java/study/querydsl/repository/Querydsl4Repository.java
@@ -3,6 +3,7 @@ package study.querydsl.repository;
 import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
@@ -35,5 +36,12 @@ public class Querydsl4Repository {
         this.entityManager = entityManager;
         this.querydsl = new Querydsl(entityManager, new PathBuilder<>(path.getType(), path.getMetadata()));
         this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    @PostConstruct
+    public void validate(){
+        Assert.notNull(entityManager, "entityManager must not be null");
+        Assert.notNull(querydsl, "querydsl must not be null");
+        Assert.notNull(queryFactory, "queryFactory must not be null");
     }
 }
